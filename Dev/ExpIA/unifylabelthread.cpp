2b7ded081fbafcf1f9952f8f9ccb66f91ce53086
samhu19891 <hsy19891228@yeah.net>
2015-12-17 15:44:18 +0800

    Some adjustment on the UnifyLabelThread

--- Dev/ExpIA/unifylabelthread.cpp
+++ Dev/ExpIA/unifylabelthread.cpp
@@ -4,9 +4,17 @@
 bool UnifyLabelThread::configure(Config::Ptr config)
 {
     config_ = config;
-    if(!config_->has("RGB_r_bin_num"))return false;
-    if(!config_->has("RGB_g_bin_num"))return false;
-    if(!config_->has("RGB_b_bin_num"))return false;
+    if(!config_->has("Color_Space"))return false;
+    if("RGB"==config_->getString("Color_Space"))
+    {
+        if(!config_->has("RGB_r_bin_num"))return false;
+        if(!config_->has("RGB_g_bin_num"))return false;
+        if(!config_->has("RGB_b_bin_num"))return false;
+    }else if("Lab"==config_->getString("Color_Space")){
+        if(!config_->has("Lab_L_bin_num"))return false;
+        if(!config_->has("Lab_a_bin_num"))return false;
+        if(!config_->has("Lab_b_bin_num"))return false;
+    }else return false;
     return true;
 }
 
@@ -70,13 +78,24 @@ void UnifyLabelThread::extract_patch_feature(DefaultMesh&mesh,arma::fvec&feature
     lwh(2) = arma::norm( box.col(0) - box.col(4) );
     if( lwh(0) < lwh(1) ) lwh.swap_rows(0,1);
     arma::fvec color_hist;
-    Feature::ColorHistogramRGB<DefaultMesh>
-            color_hist_extractor(
-                config_->getInt("RGB_r_bin_num"),
-                config_->getInt("RGB_g_bin_num"),
-                config_->getInt("RGB_b_bin_num")
-                );
-    color_hist_extractor.extract(mesh,color_hist);
+    if("RGB"==config_->getString("Color_Space"))
+    {
+        Feature::ColorHistogramRGB<DefaultMesh>
+                color_hist_extractor(
+                    config_->getInt("RGB_r_bin_num"),
+                    config_->getInt("RGB_g_bin_num"),
+                    config_->getInt("RGB_b_bin_num")
+                    );
+        color_hist_extractor.extract(mesh,color_hist);
+    }else if("Lab"==config_->getString("Color_Space")){
+        Feature::ColorHistogramLab<DefaultMesh>
+                color_hist_extractor(
+                    config_->getInt("Lab_L_bin_num"),
+                    config_->getInt("Lab_a_bin_num"),
+                    config_->getInt("Lab_b_bin_num")
+                    );
+        color_hist_extractor.extract(mesh,color_hist);
+    }
     if(config_->has("Feature_height_w"))
     {
         lwh.tail(1) *= config_->getFloat("Feature_height_w");
@@ -117,6 +136,10 @@ void UnifyLabelThread::learn()
     }
     gmm_.reset(feature_dim,max_patch_num);
     gmm_.set_means(arma::conv_to<arma::mat>::from(patch_features_[max_patch_num_index]));
+    arma::mat dcovs(feature_dim,gmm_.n_gaus());
+    dcovs.each_col() = arma::var(data,0,1);
+    dcovs += std::numeric_limits<double>::epsilon();
+    gmm_.set_dcovs(dcovs);
     gmm_.learn(data,max_patch_num,arma::eucl_dist,arma::keep_existing,10,0,1e-10,true);
 }
 
@@ -141,37 +164,50 @@ void UnifyLabelThread::assign(
         arma::urowvec& label_value
         )
 {
-    label_value = arma::urowvec(features.n_cols,arma::fill::zeros);
-    arma::rowvec label_value_p(features.n_cols);
-    label_value_p.fill( std::numeric_limits<double>::lowest() );
     arma::mat feature_mat = arma::conv_to<arma::mat>::from(features);
+    uint64_t N_gaus = gmm_.n_gaus();
+    uint64_t N_feature = feature_mat.n_cols;
+    arma::mat log_p(N_gaus,N_feature);
+    arma::uvec gaus_indices = arma::linspace<arma::uvec>(0,N_gaus-1,N_gaus);
+    arma::uvec feature_indices = arma::linspace<arma::uvec>(0,N_feature-1,N_feature);
+//    std::cerr<<"0"<<std::endl;
+    label_value = arma::urowvec(N_feature,arma::fill::zeros);
+//    std::cerr<<"1"<<std::endl;
     for( size_t gindex = 0 ; gindex < gmm_.n_gaus() ; ++gindex )
     {
-        arma::rowvec log_p = gmm_.log_p(feature_mat,gindex);
-        arma::uvec log_p_indices = arma::linspace<arma::uvec>(0,log_p.size()-1,log_p.size());
-        arma::uword max_p_index;
-        bool assigned = false;
-        while(!assigned)
-        {
-            log_p.max(max_p_index);
-            if( log_p(max_p_index) > label_value_p( log_p_indices(max_p_index) ) )
-            {
-                label_value( log_p_indices(max_p_index) ) = gindex + 1;
-                label_value_p( log_p_indices(max_p_index) ) = log_p(max_p_index);
-                assigned = true;
-            }else{
-                log_p.shed_col(max_p_index);
-                log_p_indices.shed_row(max_p_index);
-            }
-            if(log_p.is_empty())break;
-        }
-//        std::cerr<<"gindex:"<<std::endl;
-//        std::cerr<<gindex<<std::endl;
-//        if(!assigned)std::cerr<<"not assinged"<<std::endl;
-//        else std::cerr<<"assigned to "<<log_p_indices(max_p_index)<<std::endl;
-//        std::cerr<<"log_p:"<<std::endl;
-//        std::cerr<<log_p<<std::endl;
+        log_p.row(gindex) = gmm_.log_p(feature_mat,gindex);
     }
+//    std::cerr<<"2"<<std::endl;
+    uint64_t row_max_index;
+    uint64_t col_max_index;
+    uint64_t current_row = 0;
+    log_p.max(col_max_index,row_max_index);
+    current_row = col_max_index;
+    while(!log_p.is_empty())
+    {
+        while(current_row >= log_p.n_rows ) -- current_row;
+        log_p.row(current_row).max(row_max_index);
+        log_p.col(row_max_index).max(col_max_index);
+        if(current_row==col_max_index)
+        {
+            std::cerr<<gaus_indices(current_row)<<"->"<<feature_indices(row_max_index)<<std::endl;
+            label_value( feature_indices(row_max_index) ) = 1 + arma::uword(gaus_indices(current_row));
+//            std::cerr<<"a"<<std::endl;
+            log_p.shed_row(current_row);
+            gaus_indices.shed_row(current_row);
+//            std::cerr<<"c"<<std::endl;
+            log_p.shed_col(row_max_index);
+//            std::cerr<<"d"<<std::endl;
+            feature_indices.shed_row(row_max_index);
+        }else{
+            log_p.max(col_max_index,row_max_index);
+            current_row = col_max_index;
+        }
+//        std::cerr<<current_row<<std::endl;
+//        std::cerr<<"log_p.n_rows:"<<log_p.n_rows<<std::endl;
+//        std::cerr<<"log_p.n_cols:"<<log_p.n_cols<<std::endl;
+    }
+//    std::cerr<<"3"<<std::endl;
 }
 
 void UnifyLabelThread::alter_label(
