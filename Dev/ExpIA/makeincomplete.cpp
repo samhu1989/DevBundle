faace3151d57f2f99528acfdb4842bf4c45c99c4
Sam Hu <hsy19891228@yeah.net>
2017-07-08 21:40:11 +0800

    implement tool for exp

--- Dev/ExpIA/makeincomplete.cpp
+++ Dev/ExpIA/makeincomplete.cpp
@@ -1,6 +1,58 @@
 #include "makeincomplete.h"
-
-MakeIncomplete::MakeIncomplete(QObject *parent) : QThread(parent)
+#include <random>
+#include <QFileInfo>
+#include <QDir>
+MakeIncomplete::MakeIncomplete(
+        MeshBundle<DefaultMesh>::PtrList inputs,
+        QList<float> ratio,
+        QString path,
+        QObject *parent
+        ):inputs_(inputs),ratio_(ratio),path_(path),QThread(parent)
 {
+    qSort(ratio_.begin(),ratio_.end());
+}
 
+void MakeIncomplete::run(void)
+{
+    for(MeshBundle<DefaultMesh>::PtrList::iterator iter=inputs_.begin();iter!=inputs_.end();++iter)
+    {
+        saveByRatio(*iter);
+    }
+}
+
+void MakeIncomplete::saveByRatio(MeshBundle<DefaultMesh>::Ptr in)
+{
+    DefaultMesh& m = in->mesh_;
+    int N = m.n_vertices();
+    DefaultMesh om;
+    std::vector<DefaultMesh::VertexHandle> sortedV;
+    int idx = 0;
+    foreach(float r,ratio_)
+    {
+        QString tmp;
+        QString currentpath = path_ + tmp.sprintf("/%02d/",idx);
+        QDir dir(currentpath);
+        if(!dir.exists())
+        {
+            dir.mkdir("./");
+        }
+        int Nr = int(std::round(r*float(N)));
+        int N_limit = std::min(Nr,int(sortedV.size()));
+        for(int i = om.n_vertices(); i < N_limit ; ++i )
+        {
+            DefaultMesh::VertexHandle vi = sortedV[i];
+            DefaultMesh::VertexHandle vo = om.add_vertex(m.point(vi));
+            om.set_normal(vo,m.normal(vi));
+            om.set_color(vo,m.color(vi));
+        }
+        OpenMesh::IO::Options opt;
+        opt+=OpenMesh::IO::Options::Binary;
+        opt+=OpenMesh::IO::Options::VertexColor;
+        opt+=OpenMesh::IO::Options::VertexNormal;
+        QString filepath = dir.absoluteFilePath(QString::fromStdString(in->name_+".ply"));
+        if(!OpenMesh::IO::write_mesh(om,filepath.toStdString(),opt,13)){
+            std::cerr<<"can't save to:"<<filepath.toStdString()<<std::endl;
+        }
+        idx++;
+    }
 }
--- Dev/ExpIA/makeincomplete.h
+++ Dev/ExpIA/makeincomplete.h
@@ -1,15 +1,29 @@
 #ifndef MAKEINCOMPLETE_H
 #define MAKEINCOMPLETE_H
-
+#include "common.h"
 #include <QThread>
-
+#include <QString>
+#include <QList>
 class MakeIncomplete : public QThread
 {
     Q_OBJECT
 public:
-    explicit MakeIncomplete(QObject *parent = 0);
+    explicit MakeIncomplete(
+            MeshBundle<DefaultMesh>::PtrList inputs,
+            QList<float> ratio,
+            QString path,
+            QObject *parent = 0
+            );
 signals:
 public slots:
+protected slots:
+    void run(void);
+protected:
+    void saveByRatio(MeshBundle<DefaultMesh>::Ptr in);
+private:
+    MeshBundle<DefaultMesh>::PtrList inputs_;
+    QList<float> ratio_;
+    QString path_;
 };
 
 #endif // MAKEINCOMPLETE_H
